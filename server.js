e9820761da74732e010cf092c10eb856729a6aae
cbin <olegskster@gmail.com>
2016-10-31 14:11:27 +0300

    test

--- server.js
+++ server.js
@@ -61,7 +61,6 @@ dialog.matches(/^search/i, [
 bot.dialog('/', dialog);
 
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 8080, function () {
-    console.log('%s listening to %s', server.name, server.url);
+server.listen(process.env.port || process.env.PORT || 8080);
 });
 server.post('/api/messages', connector.listen());
