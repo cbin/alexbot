0a1e2045b8fbbdfd36f4980b4baae246fd54ce37
cbin <olegskster@gmail.com>
2016-11-28 09:25:10 +0300

    delete any func

--- server.js
+++ server.js
@@ -9,35 +9,6 @@ var connector = new builder.ChatConnector(
     }
 );
 var bot = new builder.UniversalBot(connector);
-/*var salesData = {
-    "west": {
-        units: 200,
-        total: "$6,000"
-    },
-    "central": {
-        units: 100,
-        total: "$3,000"
-    },
-    "east": {
-        units: 300,
-        total: "$9,000"
-    }
-};
-
-bot.dialog('/', [
-    function (session) {
-        builder.Prompts.choice(session, "Which region would you like sales for?", salesData); 
-    },
-    function (session, results) {
-        if (results.response) {
-            var region = salesData[results.response.entity];
-            session.send("We sold %(units)d units for a total of %(total)s.", region); 
-        } else {
-            session.send("ok");
-        }
-    }
-]);
-*/
 
 var dialog = new builder.IntentDialog();
 bot.dialog('/', dialog);
